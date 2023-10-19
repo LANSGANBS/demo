c599d41c9b5bc4f1be6d122d5f8bdf816017f837
LANSGANBS <2140967451@qq.com>
2023-10-19 10:33:36 +0800

    demo

--- G.可莉学除法.cpp
+++ G.可莉学除法.cpp
@@ -1,34 +1,25 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <unordered_map>
 using namespace std;
+
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
     int T;
     cin >> T;
     while (T--)
     {
         int n, x;
         cin >> n >> x;
-        vector<int> a(n);
-        for (int i = 0; i < n; i++)
-        {
-            cin >> a[i];
-        }
+        unordered_map<int, int> cnt;
         long long ans = 0;
-        map<int, int> count;
-        for (int i = 0; i < n; i++)
+        for (int i = 1; i <= n; i++)
         {
-            if (a[i] % x == 0)
-            {
-                ans += count[a[i] / x];
-            }
-            if (x == 1)
-            {
-                ans += count[a[i]];
-            }
-            count[a[i]]++;
+            int a;
+            cin >> a;
+            ans += cnt[a / x] * cnt[a * x];
+            cnt[a]++;
         }
-        cout << ans << "\n";
+        cout << ans << endl;
     }
+    return 0;
 }
